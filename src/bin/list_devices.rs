dfe9da04768dfba187a4cffdb06ce25c0466cfa9
Declan Kelly <dkelly.home@gmail.com>
2018-06-13 19:36:08 -0700

    Add device info to printout

--- src/bin/list_devices.rs
+++ src/bin/list_devices.rs
@@ -1,15 +1,52 @@
 extern crate ocl;
 
+use ocl::core::DeviceInfo;
+use ocl::enums::DeviceInfo::*;
 use ocl::{Device, Platform};
 
 // const OPENCL_SRC_FILE: &'static str = "";
 
+const DEVICE_INFO: [DeviceInfo; 14] = [
+    MaxComputeUnits,
+    MaxWorkItemDimensions,
+    MaxWorkGroupSize,
+    MaxWorkItemSizes,
+    PreferredVectorWidthChar,
+    PreferredVectorWidthShort,
+    PreferredVectorWidthInt,
+    PreferredVectorWidthLong,
+    PreferredVectorWidthFloat,
+    PreferredVectorWidthDouble,
+    Extensions,
+    LocalMemSize,
+    GlobalMemSize,
+    MaxConstantBufferSize,
+];
+
 fn main() -> ocl::Result<()> {
     for platform in Platform::list() {
-        println!("{}:", platform.name()?);
+        println!("Platform: {}", platform.name()?);
 
+        println!("Devices: ");
         for device in Device::list_all(&platform)? {
             println!("{}", device.name()?);
+            println!(
+                "Max WG: {}, IS AVAIL: {}",
+                device.max_wg_size()?,
+                device.is_available()?
+            );
+
+            println!("Info:");
+            for info_item in &DEVICE_INFO {
+                println!("{:?}: {}", info_item, device.info(*info_item)?);
+            }
+
+            println!("\n");
+        }
+
+        println!("Extensions:");
+        for extension in platform.extensions()?.iter() {
+            println!("{}", extension);
         }
 
         println!("");
